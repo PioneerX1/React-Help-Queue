ae72eebea0975a0871a0c4a79e48512fa6a59b64
Mick Sexton <lacrookedbeat@hotmail.com>
2017-10-12 11:24:38 -0700

    add propTypes for the Ticket

--- src/components/TicketList.jsx
+++ src/components/TicketList.jsx
@@ -3,7 +3,11 @@ import Ticket from "./Ticket";
 
 function TicketList(props) {
   return (
-    <Ticket />
+    <Ticket
+      location="3A"
+      names="Thato and Haley"
+      issue="Firebase won't save record"
+    />
   );
 }
 
