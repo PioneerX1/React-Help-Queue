4be448eea2f54e2dab00ec7fc76030c892288f73
Mick Sexton <lacrookedbeat@hotmail.com>
2017-10-13 11:02:36 -0700

    add binding for handleDisplayingNewTicketForm function so THIS can read it, instead of showing null

--- src/components/NewTicketControl.jsx
+++ src/components/NewTicketControl.jsx
@@ -5,10 +5,13 @@ class NewTicketControl extends React.Component {
   constructor(props) {
     super(props);
     this.state = {formVisibleOnPage: false};
+    this.handleDisplayingNewTicketForm = this.handleDisplayingNewTicketForm.bind(this);
   }
 
   handleDisplayingNewTicketForm(event) {
     console.log("New ticket button was clicked!");
+    this.setState({formVisibleOnPage: true});
+    console.log("formVisible set to: " + this.state.formVisibleOnPage);
   }
 
   render() {
